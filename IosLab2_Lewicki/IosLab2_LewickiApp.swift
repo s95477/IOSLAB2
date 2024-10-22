47000a66079b75d73cc5dbef79cf4ee4e765fe20
s95477 <s95477@pollub.edu.pl>
2024-10-22 12:08:09 +0200

    Initial Commit

---
+++ IosLab2_Lewicki/IosLab2_LewickiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosLab2_LewickiApp.swift
+//  IosLab2_Lewicki
+//
+//  Created by student on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct IosLab2_LewickiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
